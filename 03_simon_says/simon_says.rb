6c03260eeaafde94e37f60bf042b6a606c98ed3e
Bosco So <bosco@sfruby.info>
2014-06-21 13:00:57 -0700

    finished implementation of simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -9,3 +9,35 @@ end
 def repeat(message, count=2)
   ([message] * count).join(" ")
 end 
+
+def start_of_word(word, count)
+  word[0, count]
+end
+
+def first_word(sentence)
+  sentence.split.first
+end
+
+def titleize(sentence)
+  words = sentence.split
+
+  # always capitalize the first word
+  first_word = words.shift.capitalize
+
+  # subject subsequent words to exclusion rule
+  exclusions = {
+    # articles
+    'the' => true,
+
+    # conjunctions
+    'and' => true,
+
+    # prepositions
+    'over' => true,
+  }
+  capitalized = words.map {|word| exclusions.has_key?(word) ? word : word.capitalize }
+
+  # add first word back to the front of the array
+  capitalized.unshift(first_word)
+  capitalized.join(' ')
+end
